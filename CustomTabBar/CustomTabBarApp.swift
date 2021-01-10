36043f6a9ea82aa60804aeb591782fcfdbf85402
Joep Hinderink <hello@joephinderink.nl>
2021-01-10 13:52:06 +0100

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Joep Hinderink on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
